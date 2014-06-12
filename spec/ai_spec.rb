b8025e6f25c5afacb3ae4f4462fbc16f1938b179
Shawn Klein <zappeln@gmail.com>
2014-06-11 19:36:35 -0500

    created basic method and tests for computerai class

--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -6,8 +6,18 @@ describe ComputerAi do
 
   let(:computer)  {ComputerAi.new}
 
-  it "should determine the next position to move to" do
+  # it "should determine the next position to move to" do
+  #   board = ["x", "x", nil, "o", nil, "o", nil, nil, nil]
+  #   computer.evaluate_board(board, "x", "o").should eq(2)
+  # end
+
+  # it "should create a value based on board win" do
+  #   board = ["x", "x", nil, "o", nil, "o", nil, nil, nil]
+  #   computer.create_value.should eq(2)
+  # end
+
+  it "should determine if board is complete" do
     board = ["x", "x", nil, "o", nil, "o", nil, nil, nil]
-    computer.evaluate_board(board).should eq(2)
+    computer.board_complete?(board).should eq(false)
   end
 end
